50eb1fdb70f746e51a838342c78abb12ec000416
htascilar <htascilar@hotmail.com>
2025-07-18 20:49:47 +0200

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,11 +1,12 @@
-// Firebase config (replace with your actual config)
+// Firebase Configuration (compat style)
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC9trnGDg84wqZQN3T2K26TLLkYvmP4ZLY",
+  authDomain: "htasci-30698.firebaseapp.com",
+  projectId: "htasci-30698",
+  storageBucket: "htasci-30698.firebasestorage.app",
+  messagingSenderId: "709615242534",
+  appId: "1:709615242534:web:ac0bc14fc24855917645e7",
+  measurementId: "G-7VSNND2871"
 };
 
 firebase.initializeApp(firebaseConfig);
